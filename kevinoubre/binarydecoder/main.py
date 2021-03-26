706d9704de50035cc6797381d6f2999e80774fae
kevin <kjo018@latech.edu>
2021-03-26 11:39:56 -0500

    Documented my binary decoder

--- kevinoubre/binarydecoder/main.py
+++ kevinoubre/binarydecoder/main.py
@@ -12,32 +12,29 @@ def divideIntoNBits(l, n):
     for i in range(0, len(l), n):  
         yield l[i:i + n] 
   
-# Checks to see if size is right
-def checkValidSeq(l,n):
-    for val in l:
-        if len(l) != n:
-            return False
-    
-    return True
-
 # Turns binary data to ascii
 def binaryToAsciiChar(l):
     for val in l:
         yield chr(int(val,2))
 
+""" 
+CODE-FLOW:
+* Split string into 7/8 bits list [bitsSeven/bitsEight]
+* Use the function binaryToAscii to turn the bits to an array of ascii characters
+* Use "".join() to combine the list together to make a single string
+* print with prGreen to print green colors
+"""
 
+# Loop through stdin
 for line in sys.stdin:
     line = line.strip('\n')
     bitsSeven = list(divideIntoNBits(line,7))
     bitsEight = list(divideIntoNBits(line,8))
-    # print(bitsSeven)
-    # print(bitsEight)
+    # Combine the list iterable to create a string
     data = "".join(list(binaryToAsciiChar(bitsEight)))
     print("\n\n")
     prGreen("8-bit val:\n{}".format(data))
-    # print("8-bit val:\t{}".format(data))
-    print("~~~")
+    print("-----------------------------")
     data = "".join(list(binaryToAsciiChar(bitsSeven)))
     prRed("7-bit val:\n{}".format(data))
-    # print("7-bit val:\t{}".format(data))
     
